80d6d37b534edb4433ebfb6e6e623a9d429dfff4
azamat-110 <tursunbayevazamat565@gmail.com>
2025-05-17 17:00:35 +0500

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,10 @@ import AboutPage from "@/pages/AboutPage.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {path : '/', redirect: '/launches'},
-    {path : '/launches', component: LaunchPage},
-    {path: '/about', component: AboutPage},
+    { path: '/', redirect: '/launches' },
+    { path: '/launches', component: LaunchPage },
+    { path: '/about', component: AboutPage },
   ],
 });
 
-export default router
+export default router;
