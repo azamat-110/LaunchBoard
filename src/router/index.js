d05d9b5dad14e77a6f10cbb1794f805e7280b6b4
azamat-110 <tursunbayevazamat565@gmail.com>
2025-05-17 11:45:30 +0500

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import LaunchPage from "@/pages/LaunchPage.vue";
 import AboutPage from "@/pages/AboutPage.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
         {path: '/', redirect: '/launches'},
         {path: '/launches', component: LaunchPage},
