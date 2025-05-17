a4c407aa88f747a37d2668050b4c82a6899c32b3
azamat-110 <tursunbayevazamat565@gmail.com>
2025-05-17 18:45:58 +0500

    hash

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/LaunchBoard/' : '/',
+  base: '/LaunchBoard/',
   plugins: [
     vue(),
   ],
